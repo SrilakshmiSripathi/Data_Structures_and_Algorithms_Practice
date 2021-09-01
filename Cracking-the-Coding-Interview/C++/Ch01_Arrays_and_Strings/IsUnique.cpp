fb441f985b220c1e9a5dbcd8f10cbe31b951629d
SrilakshmiSripathi <43179518+SrilakshmiSripathi@users.noreply.github.com>
2021-08-31 22:04:10 -0400

    ..

---
+++ Cracking-the-Coding-Interview/C++/Ch01_Arrays_and_Strings/IsUnique.cpp
@@ -0,0 +1,3 @@
+import cout;
+
+cout<<"print"
---
+++ Cracking-the-Coding-Interview/Java/Ch01_Arrays_and_Strings/IsUnique.java
@@ -0,0 +1,25 @@
+
+
+/**
+ * Does a string have all unique characters
+ * @author Srilakshmi Sripathi
+ */
+
+
+pubic class UniqueCharString{
+    public status boolean isUnique(String str){
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+    
+    
+}
+
+
+
